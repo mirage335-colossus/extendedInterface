d1801cfd4c9aa2a8d8332480f26b2393c0094c77
mirage335 <spamfreemirage335@gmail.com>
2020-02-17 19:56:30 -0500

    Hook improvements, continued use.

--- support/steamvr/hook.bat
+++ support/steamvr/hook.bat
@@ -1,3 +1,3 @@
 timeout /NOBREAK 3
-CALL "C:\core\infrastructure\extendedInterface\support\steamvr\hook_lock.bat" "%1"
+CALL "C:\core\infrastructure\extendedInterface\support\steamvr\hook_lock.bat" %1
 

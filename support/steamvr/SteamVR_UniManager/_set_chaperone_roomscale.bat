9507b6d174adec168ae1d3640bccad88a5c45429
mirage335 <spamfreemirage335@gmail.com>
2020-12-23 13:49:28 -0500

    Disable - BROKEN - SteamVR_UniManager .

--- support/steamvr/SteamVR_UniManager/_set_chaperone_roomscale.bat
+++ support/steamvr/SteamVR_UniManager/_set_chaperone_roomscale.bat
@@ -1,3 +1,8 @@
+
+REM BROKEN!
+exit /b 0
+
+
 cd "C:\Program Files (x86)\Steam\config\"
 copy /Y "chaperone_info.vrchap" "chaperone_info.vrchap.roomscale"
 
--- support/steamvr/SteamVR_UniManager/roomscale.bat
+++ support/steamvr/SteamVR_UniManager/roomscale.bat
@@ -1,3 +1,8 @@
+
+REM BROKEN!
+exit /b 0
+
+
 cd "C:\Program Files (x86)\Steam\config\"
 copy "chaperone_info.vrchap.roomscale" "chaperone_info.vrchap"
 

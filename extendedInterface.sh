4c30dc818ba198e51d2706453c42b04ffd43f8c6
mirage335 <spamfreemirage335@gmail.com>
2023-07-14 05:27:42 -0500

    Draft.

--- extendedInterface.sh
+++ extendedInterface.sh
@@ -4855,6 +4855,507 @@ _package() {
 
 
 
+_configureLocal() {
+	_configureFile "$1" "_local"
+}
+
+_configureFile() {
+	cp "$scriptAbsoluteFolder"/"$1" "$scriptAbsoluteFolder"/"$2"
+}
+
+_configureOps() {
+	echo "$@" >> "$scriptAbsoluteFolder"/ops
+}
+
+_resetOps() {
+	rm "$scriptAbsoluteFolder"/ops
+}
+
+_gitPull_ubiquitous() {
+	#git pull
+	_gitBest pull
+}
+
+_gitClone_ubiquitous() {
+	#git clone --depth 1 git@github.com:mirage335/ubiquitous_bash.git
+	_gitBest clone --depth 1 git@github.com:mirage335/ubiquitous_bash.git
+}
+
+_selfCloneUbiquitous() {
+	"$scriptBin"/.ubrgbin.sh _ubrgbin_cpA "$scriptBin" "$ubcoreUBdir"/ > /dev/null 2>&1
+	cp -a "$scriptAbsoluteLocation" "$ubcoreUBdir"/lean.sh
+	cp -a "$scriptAbsoluteLocation" "$ubcoreUBdir"/ubcore.sh
+	cp -a "$scriptAbsoluteFolder"/lean.sh "$ubcoreUBdir"/lean.sh > /dev/null 2>&1
+	cp -a "$scriptAbsoluteFolder"/lean.sh "$ubcoreUBdir"/ubcore.sh > /dev/null 2>&1
+	cp -a "$scriptAbsoluteFolder"/ubcore.sh "$ubcoreUBdir"/ubcore.sh > /dev/null 2>&1
+	cp -a "$scriptAbsoluteLocation" "$ubcoreUBdir"/ubiquitous_bash.sh
+	
+	cp -a "$scriptAbsoluteFolder"/lean.py "$ubcoreUBdir"/lean.py > /dev/null 2>&1
+}
+
+_installUbiquitous() {
+	local localFunctionEntryPWD
+	localFunctionEntryPWD="$PWD"
+	
+	cd "$ubcoreDir"
+	
+	cd "$ubcoreUBdir"
+	_messagePlain_nominal 'attempt: git pull'
+	if [[ "$nonet" != "true" ]] && type git > /dev/null 2>&1
+	then
+		_gitBest_detect
+		
+		local ub_gitPullStatus
+		#git pull
+		_gitBest pull
+		ub_gitPullStatus="$?"
+		#[[ "$ub_gitPullStatus" != 0 ]] && git pull && ub_gitPullStatus="$?"
+		[[ "$ub_gitPullStatus" != 0 ]] && _gitBest pull && ub_gitPullStatus="$?"
+		! cd "$localFunctionEntryPWD" && return 1
+		
+		[[ "$ub_gitPullStatus" == "0" ]] && _messagePlain_good 'pass: git pull' && cd "$localFunctionEntryPWD" && return 0
+	fi
+	_messagePlain_warn 'fail: git pull'
+	
+	cd "$ubcoreDir"
+	_messagePlain_nominal 'attempt: git clone'
+	[[ "$nonet" != "true" ]] && type git > /dev/null 2>&1 && [[ ! -e ".git" ]] && [[ ! -e "$ubcoreUBdir"/.git ]] && _gitClone_ubiquitous && _messagePlain_good 'pass: git clone' && return 0
+	[[ "$nonet" != "true" ]] && type git > /dev/null 2>&1 && [[ ! -e ".git" ]] && [[ ! -e "$ubcoreUBdir"/.git ]] && _gitClone_ubiquitous && _messagePlain_good 'pass: git clone' && return 0
+	_messagePlain_warn 'fail: git clone'
+	
+	cd "$ubcoreUBdir"
+	_messagePlain_nominal 'attempt: self git pull'
+	# WARNING: Not attempted if 'nonet' has been set 'true', due to possible conflicts with scripts intending only to copy one file (ie. by SSH transfer).
+	if [[ "$nonet" != "true" ]] && type git > /dev/null 2>&1 && [[ -e "$scriptAbsoluteFolder"/.git ]] && [[ -e "$scriptAbsoluteFolder"/.git ]]
+	then
+		
+		local ub_gitPullStatus
+		#git reset --hard
+		[[ -e "$scriptAbsoluteFolder"/lean.sh ]] && rm -f "$ubcoreUBdir"/lean.sh > /dev/null 2>&1
+		[[ -e "$scriptAbsoluteFolder"/ubcore.sh ]] && rm -f "$ubcoreUBdir"/ubcore.sh > /dev/null 2>&1
+		[[ -e "$scriptAbsoluteFolder"/ubiquitous_bash.sh ]] && rm -f "$ubcoreUBdir"/ubiquitous_bash.sh > /dev/null 2>&1
+		[[ -e "$scriptAbsoluteFolder"/lean_compressed.sh ]] && rm -f "$ubcoreUBdir"/lean_compressed.sh > /dev/null 2>&1
+		[[ -e "$scriptAbsoluteFolder"/core_compressed.sh ]] && rm -f "$ubcoreUBdir"/core_compressed.sh > /dev/null 2>&1
+		[[ -e "$scriptAbsoluteFolder"/ubcore_compressed.sh ]] && rm -f "$ubcoreUBdir"/ubcore_compressed.sh > /dev/null 2>&1
+		[[ -e "$scriptAbsoluteFolder"/ubiquitous_bash_compressed.sh ]] && rm -f "$ubcoreUBdir"/ubiquitous_bash_compressed.sh > /dev/null 2>&1
+		[[ -e "$scriptAbsoluteFolder"/lean.py ]] && rm -f "$ubcoreUBdir"/lean.py > /dev/null 2>&1
+		#git reset --hard
+		#git pull "$scriptAbsoluteFolder"
+		_gitBest pull "$scriptAbsoluteFolder"
+		_gitBest reset --hard
+		ub_gitPullStatus="$?"
+		! cd "$localFunctionEntryPWD" && return 1
+		
+		[[ "$ub_gitPullStatus" == "0" ]] && _messagePlain_good 'pass: self git pull' && cd "$localFunctionEntryPWD" && return 0
+	fi
+	_messagePlain_warn 'fail: self git pull'
+	
+	cd "$ubcoreDir"
+	_messagePlain_nominal 'attempt: self clone'
+	[[ -e ".git" ]] && _messagePlain_bad 'fail: self clone' && return 1
+	_selfCloneUbiquitous && return 0
+	_messagePlain_bad 'fail: self clone' && return 1
+	
+	return 0
+	
+	cd "$localFunctionEntryPWD"
+}
+
+
+
+
+_setupUbiquitous() {
+	_messageNormal "init: setupUbiquitous"
+	
+	if _if_cygwin
+	then
+		echo 'detected: cygwin'
+		_messagePlain_probe_cmd git config --global core.autocrlf input
+		_messagePlain_probe_cmd git config --global core.eol lf
+	fi
+	
+	_force_cygwin_symlinks
+	_if_cygwin && _setup_ubiquitousBash_cygwin_procedure
+	
+	
+	local ubHome
+	ubHome="$HOME"
+	[[ "$1" != "" ]] && ubHome="$1"
+	
+	export ubcoreDir="$ubHome"/.ubcore
+	export ubcoreFile="$ubcoreDir"/.ubcorerc
+	
+	export ubcoreDir_accessories="$ubHome"/.ubcore/accessories
+	
+	
+	# WARNING: Despite the name, do NOT point this to 'ubcore.sh' or similar. Full set of functions are expected from this file by some use cases!
+	export ubcoreUBdir="$ubcoreDir"/ubiquitous_bash
+	export ubcoreUBfile="$ubcoreDir"/ubiquitous_bash/ubiquitous_bash.sh
+	
+	_messagePlain_probe 'ubHome= '"$ubHome"
+	_messagePlain_probe 'ubcoreDir= '"$ubcoreDir"
+	_messagePlain_probe 'ubcoreFile= '"$ubcoreFile"
+	
+	_messagePlain_probe 'ubcoreUBdir= '"$ubcoreUBdir"
+	_messagePlain_probe 'ubcoreUBfile= '"$ubcoreUBfile"
+	
+	mkdir -p "$ubcoreUBdir"
+	! [[ -e "$ubcoreUBdir" ]] && _messagePlain_bad 'missing: ubcoreUBdir= '"$ubcoreUBdir" && _messageFAIL && return 1
+	
+	mkdir -p "$ubcoreDir_accessories"
+	! [[ -e "$ubcoreDir_accessories" ]] && _messagePlain_bad 'missing: ubcoreUBdir_accessories= '"$ubcoreDir_accessories" && _messageFAIL && return 1
+	
+	
+	_messageNormal "install: setupUbiquitous"
+	! _installUbiquitous && _messageFAIL && return 1
+	! [[ -e "$ubcoreUBfile" ]] && _messagePlain_bad 'missing: ubcoreUBfile= '"$ubcoreUBfile" && _messageFAIL && return 1
+	
+	
+	_messageNormal "hook: setupUbiquitous"
+	if ! _permissions_ubiquitous_repo "$ubcoreUBdir" && _messagePlain_bad 'permissions: ubcoreUBdir = '"$ubcoreUBdir"
+	then
+		if ! _if_cygwin
+		then
+			_messageFAIL
+			return 1
+		else
+			echo 'warn: accepted: cygwin: permissions'
+		fi
+	fi
+	
+	mkdir -p "$ubHome"/bin/
+	ln -sf "$ubcoreUBfile" "$ubHome"/bin/ubiquitous_bash.sh
+	ln -sf "$ubcoreUBfile" "$ubHome"/bin/_winehere
+	ln -sf "$ubcoreUBfile" "$ubHome"/bin/_winecfghere
+	
+	echo '#!/bin/bash
+"$HOME"/bin/ubiquitous_bash.sh _vncf "$@"' > "$ubHome"/bin/vncf
+	chmod u+x "$ubHome"/bin/vncf
+	
+	echo '#!/bin/bash
+"$HOME"/bin/ubiquitous_bash.sh _sshf "$@"' > "$ubHome"/bin/sshf
+	chmod u+x "$ubHome"/bin/vncf
+	
+	
+	
+	_setupUbiquitous_here > "$ubcoreFile"
+	_setupUbiquitous_accessories_bashrc >> "$ubcoreFile"
+	! [[ -e "$ubcoreFile" ]] && _messagePlain_bad 'missing: ubcoreFile= '"$ubcoreFile" && _messageFAIL && return 1
+	
+	
+	! grep ubcore "$ubHome"/.bashrc > /dev/null 2>&1 && _messagePlain_probe "$ubcoreFile"' >> '"$ubHome"/.bashrc && echo ". ""$ubcoreFile" >> "$ubHome"/.bashrc
+	! grep ubcore "$ubHome"/.bashrc > /dev/null 2>&1 && _messagePlain_bad 'missing: bashrc hook' && _messageFAIL && return 1
+	
+	
+	if [[ ! -e "$HOME"/.bash_profile ]] || ! grep '\.bashrc' "$HOME"/.bash_profile > /dev/null 2>&1
+	then
+		_setupUbiquitous_bashProfile_here >> "$HOME"/.bash_profile
+	fi
+	
+	
+	
+	
+	_messageNormal "install: setupUbiquitous_accessories"
+	
+	_setupUbiquitous_accessories "$@"
+	
+	
+	_messageNormal "request: setupUbiquitous_accessories , setupUbiquitous"
+	
+	_setupUbiquitous_accessories_requests "$@"
+	
+	if ! _if_cygwin
+	then
+		# WARNING: End user file association. Do NOT call within scripts.
+		# WARNING: Necessarily relies on a 'deprecated' 'field code' with the 'Exec key' of a 'Desktop Entry' file association.
+		# https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
+		_messagePlain_request 'association: *.bat'
+		echo 'konsole --workdir %d -e /bin/bash %f (open in graphical terminal emulator from file manager) (preferred)'
+		echo "bash ('Advanced Options -> Run in terminal')"
+	fi
+	
+	_messagePlain_request "Now import new functionality into current shell if not in current shell."
+	if [[ "$profileScriptLocation" != "" ]] && [[ "$profileScriptFolder" != "" ]]
+	then
+		_messagePlain_request ". "'"'"$scriptAbsoluteLocation"'"' --profile _importShortcuts
+	else
+		_request_visualPrompt
+	fi
+	
+	sleep 3
+	return 0
+}
+
+_setupUbiquitous_nonet() {
+	local oldNoNet
+	oldNoNet="$nonet"
+	export nonet="true"
+	_setupUbiquitous "$@"
+	[[ "$oldNoNet" != "true" ]] && export nonet="$oldNoNet"
+}
+
+_upgradeUbiquitous() {
+	_setupUbiquitous
+}
+
+_resetUbiquitous_sequence() {
+	_start scriptLocal_mkdir_disable
+	
+	[[ ! -e "$HOME"/.bashrc ]] && return 0
+	cp "$HOME"/.bashrc "$HOME"/.bashrc.bak
+	cp "$HOME"/.bashrc "$safeTmp"/.bashrc
+	grep -v 'ubcore' "$safeTmp"/.bashrc > "$safeTmp"/.bashrc.tmp
+	mv "$safeTmp"/.bashrc.tmp "$HOME"/.bashrc
+	
+	[[ ! -e "$HOME"/.ubcore ]] && return 0
+	rm "$HOME"/.ubcorerc
+	
+	_stop
+}
+
+_resetUbiquitous() {
+	"$scriptAbsoluteLocation" _resetUbiquitous_sequence
+}
+
+_refresh_anchors_ubiquitous() {
+	#cp -a "$scriptAbsoluteFolder"/_anchor "$scriptAbsoluteFolder"/_ubide
+	
+	cp -a "$scriptAbsoluteFolder"/_anchor "$scriptAbsoluteFolder"/_ubdb
+	
+	cp -a "$scriptAbsoluteFolder"/_anchor "$scriptAbsoluteFolder"/_test
+	cp -a "$scriptAbsoluteFolder"/_anchor "$scriptAbsoluteFolder"/_true
+	cp -a "$scriptAbsoluteFolder"/_anchor "$scriptAbsoluteFolder"/_false
+	
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_test.bat
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_true.bat
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_false.bat
+	
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_bin.bat
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_bash.bat
+	
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_setup_ubcp.bat
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_setup_ubiquitousBash_cygwin.bat
+	
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_setupUbiquitous.bat
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_setupUbiquitous_nonet.bat
+	
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_demand_broadcastPipe_page.bat
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_terminate_broadcastPipe_page.bat
+	
+	
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_packetDriveDevice.bat
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_packetDriveDevice_remove.bat
+}
+
+
+_refresh_anchors_cautossh() {
+	cp -a "$scriptAbsoluteFolder"/_anchor "$scriptAbsoluteFolder"/_test
+	cp -a "$scriptAbsoluteFolder"/_anchor "$scriptAbsoluteFolder"/_setup
+	cp -a "$scriptAbsoluteFolder"/_anchor "$scriptAbsoluteFolder"/_bash
+	cp -a "$scriptAbsoluteFolder"/_anchor "$scriptAbsoluteFolder"/_bin
+	cp -a "$scriptAbsoluteFolder"/_anchor "$scriptAbsoluteFolder"/_grsync
+	
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_test.bat
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_setup.bat
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_bash.bat
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_bin.bat
+	cp -a "$scriptAbsoluteFolder"/_anchor.bat "$scriptAbsoluteFolder"/_grsync.bat
+}
+
+
+# EXAMPLE ONLY.
+# _refresh_anchors() {
+# 	cp -a "$scriptAbsoluteFolder"/_anchor "$scriptAbsoluteFolder"/_true
+# }
+
+
+# CAUTION: Anchor scripts MUST include code to ignore '--' suffix specific software name convention!
+# CAUTION: ONLY intended to be used either with generic software, or anchors following '--' suffix specific software name convention!
+# WARNING: DO NOT enable in "core.sh". Intended to be enabled by "_local/ops.sh".
+# ATTENTION: Set "$ub_anchor_specificSoftwareName" or similar in "ops.sh".
+# ATTENTION: Set ub_anchor_user='true' or similar in "ops.sh".
+#export ub_anchor_specificSoftwareName='experimental'
+#export ub_anchor_user="true"
+_set_refresh_anchors_specific() {
+	export ub_anchor_suffix=
+	export ub_anchor_suffix
+	
+	[[ "$ub_anchor_specificSoftwareName" == "" ]] && return 0
+	
+	export ub_anchor_suffix='--'"$ub_anchor_specificSoftwareName"
+	
+	return 0
+}
+
+_refresh_anchors_specific_single_procedure() {
+	[[ "$ub_anchor_specificSoftwareName" == "" ]] && return 1
+	
+	_set_refresh_anchors_specific
+	
+	cp -a "$scriptAbsoluteFolder"/_anchor "$scriptAbsoluteFolder"/"$1""$ub_anchor_suffix"
+	
+	return 0
+}
+# Assumes user has included "$HOME"/bin in their "$PATH".
+_refresh_anchors_user_single_procedure() {
+	[[ "$ub_anchor_user" != 'true' ]] && return 1
+	
+	_set_refresh_anchors_specific
+	! mkdir -p "$HOME"/bin && return 1
+	
+	
+	# WARNING: Default to replacement. Rare case considered acceptable for several reasons.
+	# Negligible damage potential - all replaced files are symlinks or anchors.
+	# Limited to specifically named anchor symlinks, defined in "_associate_anchors_request", typically overloaded with 'core.sh' or similar.
+	# Usually requested 'manually' through "_setup" or "_anchor", even if called through a multi-installation request.
+	# Incorrectly calling a moved, uninstalled, or otherwise incorrect previous version, of linked software, is anticipated to be a more commonly impose greater risk.
+	#ln -s "$scriptAbsoluteFolder"/"$1""$ub_anchor_suffix" "$HOME"/bin/ > /dev/null 2>&1
+	ln -sf "$scriptAbsoluteFolder"/"$1""$ub_anchor_suffix" "$HOME"/bin/
+	
+	return 0
+}
+
+# ATTENTION: Overload with 'core.sh' or similar.
+# # EXAMPLE ONLY.
+# _refresh_anchors_specific() {
+# 	_refresh_anchors_specific_single_procedure _true
+# }
+# # EXAMPLE ONLY.
+# _refresh_anchors_user() {
+# 	_refresh_anchors_user_single_procedure _true
+# }
+
+
+# ATTENTION: Overload with 'core'sh' or similar.
+# _associate_anchors_request() {
+# 	if type "_refresh_anchors_user" > /dev/null 2>&1
+# 	then
+# 		_tryExec "_refresh_anchors_user"
+# 		#return
+# 	fi
+# 	
+# 	_messagePlain_request 'association: dir'
+# 	echo _scope_konsole"$ub_anchor_suffix"
+# 	
+# 	_messagePlain_request 'association: dir'
+# 	echo _scope_designer_designeride"$ub_anchor_suffix"
+# 	
+# 	_messagePlain_request 'association: dir, *.ino'
+# 	echo _designer_generate"$ub_anchor_suffix"
+# }
+
+
+
+# ATTENTION: Overload with 'core.sh' or similar.
+# WARNING: May become default behavior.
+_anchor_autoupgrade() {
+	local currentScriptBaseName
+	currentScriptBaseName=$(basename $scriptAbsoluteLocation)
+	[[ "$currentScriptBaseName" != "ubiquitous_bash.sh" ]] && return 1
+	
+	[[ "$ub_anchor_autoupgrade" != 'true' ]] && return 0
+	
+	_findUbiquitous
+	
+	[[ -e "$ubiquitousLibDir"/_anchor ]] && cp -a "$ubiquitousLibDir"/_anchor "$scriptAbsoluteFolder"/_anchor
+}
+
+_anchor_configure() {
+	export ubAnchorTemplateCurrent="$scriptAbsoluteFolder"/_anchor
+	[[ "$1" != "" ]] && export ubAnchorTemplateCurrent="$1"
+	
+	! [[ -e "$ubAnchorTemplateCurrent" ]] && return 1
+	
+	#https://superuser.com/questions/450868/what-is-the-simplest-scriptable-way-to-check-whether-a-shell-variable-is-exporte
+	! [ "$(bash -c 'echo ${objectName}')" ] && return 1
+	
+	
+	rm -f "$scriptAbsoluteFolder"/_anchor.tmp "$scriptAbsoluteFolder"/_anchor.tmp1 "$scriptAbsoluteFolder"/_anchor.tmp2 > /dev/null 2>&1
+	cp "$ubAnchorTemplateCurrent" "$scriptAbsoluteFolder"/_anchor.tmp
+	cp "$ubAnchorTemplateCurrent" "$scriptAbsoluteFolder"/_anchor.tmp1
+	cp "$ubAnchorTemplateCurrent" "$scriptAbsoluteFolder"/_anchor.tmp2
+	
+	cat "$scriptAbsoluteFolder"/_anchor.tmp  | sed 's/^export anchorSourceDir\=.*$/export anchorSourceDir\=\"'"$objectName"'\"/g' > "$scriptAbsoluteFolder"/_anchor.tmp1
+	#perl -p -e 's/export anchorSourceDir=.*/export anchorSourceDir="$ENV{objectName}"/g' "$scriptAbsoluteFolder"/_anchor.tmp > "$scriptAbsoluteFolder"/_anchor.tmp1
+	
+	cat "$scriptAbsoluteFolder"/_anchor.tmp1 | sed 's/^SET \"MSWanchorSourceDir\=.*$/SET \"MSWanchorSourceDir\='"$objectName"'\"/g' > "$scriptAbsoluteFolder"/_anchor.tmp2
+	#perl -p -e 's/SET "MSWanchorSourceDir=.*/SET "MSWanchorSourceDir=$ENV{objectName}"/g' "$scriptAbsoluteFolder"/_anchor.tmp1 > "$scriptAbsoluteFolder"/_anchor.tmp2
+	
+	local currentScriptBaseName
+	currentScriptBaseName=$(basename $scriptAbsoluteLocation)
+	
+	# ATTENTION: Configure with 'ops.sh' , 'core.sh' , or similar.
+	if [[ "$scriptAbsoluteLocation" == *"cautossh" ]] || [[ "$scriptAbsoluteLocation" != *"ubiquitous_bash.sh" ]]
+	then
+		cat "$scriptAbsoluteFolder"/_anchor.tmp2  | sed 's/^export anchorSource\=.*$/export anchorSource\=\"'"$currentScriptBaseName"'\"/g' > "$scriptAbsoluteFolder"/_anchor.tmp3
+		#perl -p -e 's/export anchorSource=.*/export anchorSource="cautossh"/g' "$scriptAbsoluteFolder"/_anchor.tmp2 > "$scriptAbsoluteFolder"/_anchor.tmp3
+		
+		cat "$scriptAbsoluteFolder"/_anchor.tmp3 | sed 's/^SET \"MSWanchorSource\=.*$/SET \"MSWanchorSource\='"$currentScriptBaseName"'\"/g' > "$scriptAbsoluteFolder"/_anchor.tmp4
+		#perl -p -e 's/SET "MSWanchorSource=.*/SET "MSWanchorSource=cautossh"/g' "$scriptAbsoluteFolder"/_anchor.tmp3 > "$scriptAbsoluteFolder"/_anchor.tmp4
+	else
+		cat "$scriptAbsoluteFolder"/_anchor.tmp2 > "$scriptAbsoluteFolder"/_anchor.tmp4
+	fi
+	
+	mv "$scriptAbsoluteFolder"/_anchor.tmp4 "$ubAnchorTemplateCurrent"
+	chmod u+x "$ubAnchorTemplateCurrent"
+	rm -f "$scriptAbsoluteFolder"/_anchor.tmp "$scriptAbsoluteFolder"/_anchor.tmp1 "$scriptAbsoluteFolder"/_anchor.tmp2 "$scriptAbsoluteFolder"/_anchor.tmp3 "$scriptAbsoluteFolder"/_anchor.tmp4 > /dev/null 2>&1
+}
+
+
+_anchor() {
+	_anchor_autoupgrade
+	
+	_anchor_configure
+	_anchor_configure "$scriptAbsoluteFolder"/_anchor.bat
+
+	_tryExec "_anchor_special"
+	
+	! [[ -e "$scriptAbsoluteFolder"/_anchor ]] && ! [[ -e "$scriptAbsoluteFolder"/_anchor.bat ]] && return 1
+	
+	[[ "$scriptAbsoluteFolder" == *"ubiquitous_bash" ]] && _refresh_anchors_ubiquitous
+	
+	if type "_refresh_anchors_cautossh" > /dev/null 2>&1 && [[ "$scriptAbsoluteLocation" == *"cautossh" ]]
+	then
+		_tryExec "_refresh_anchors_cautossh"
+		#return
+	fi
+	
+	
+	if type "_refresh_anchors" > /dev/null 2>&1
+	then
+		_tryExec "_refresh_anchors"
+		#return
+	fi
+	
+	# CAUTION: Anchor scripts MUST include code to ignore '--' suffix specific software name convention!
+	# WARNING: DO NOT enable in "core.sh". Intended to be enabled by "_local/ops.sh".
+	if type "_refresh_anchors_specific" > /dev/null 2>&1
+	then
+		_tryExec "_refresh_anchors_specific"
+		#return
+	fi
+	
+	# CAUTION: ONLY intended to be used either with generic software, or anchors following '--' suffix specific software name convention!
+	# WARNING: DO NOT enable in "core.sh". Intended to be enabled by "_local/ops.sh".
+	if type "_refresh_anchors_user" > /dev/null 2>&1
+	then
+		_tryExec "_refresh_anchors_user"
+		#return
+	fi
+	
+	# WARNING: Calls _refresh_anchors_user . Same variables required to enable, intended to be set by "_local/ops.sh".
+	#if type "_associate_anchors_request" > /dev/null 2>&1
+	#then
+		#_tryExec "_associate_anchors_request"
+		##return
+	#fi
+	
+	
+	
+	return 0
+}
+
+
 #####Program
 
 #Typically launches an application - ie. through virtualized container.

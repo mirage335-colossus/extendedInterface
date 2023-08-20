516b7fb7f60d37b8e73fc7e0fb265dd0cb66d410
mirage335 <spamfreemirage335@gmail.com>
2023-08-20 03:30:32 -0400

    Compile.

--- extendedInterface-lean.sh
+++ extendedInterface-lean.sh
@@ -36,7 +36,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='2591634041'
-export ub_setScriptChecksum_contents='4147577457'
+export ub_setScriptChecksum_contents='2055385954'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -13280,36 +13280,36 @@ _build_extendedInterface-fetch() {
     mkdir -p "$currentAccessoriesDir"/parts/extendedInterface
     cd "$currentAccessoriesDir"/parts/extendedInterface
     cp -a "$scriptAbsoluteFolder"/.git ./
-    git config gc.pruneExpire now
-    git config gc.reflogExpire now
-    git config gc.reflogExpireUnreachable now
+    #git config gc.pruneExpire now
+    #git config gc.reflogExpire now
+    #git config gc.reflogExpireUnreachable now
     git reset --hard
     git submodule update
-    git gc --aggressive
+    #git gc --aggressive
 
-    git show-ref -s HEAD > $(git rev-parse --git-dir)/shallow
-    git reflog expire --expire-unreachable=now --all
-    git reflog expire --expire=0
-    git reflog expire --expire=now --all
-    git prune
-    git prune-packed
+    #git show-ref -s HEAD > $(git rev-parse --git-dir)/shallow
+    #git reflog expire --expire-unreachable=now --all
+    #git reflog expire --expire=0
+    #git reflog expire --expire=now --all
+    #git prune
+    #git prune-packed
 
     cd "$currentAccessoriesDir"/parts/extendedInterface/_lib/ubiquitous_bash/
-    git show-ref -s HEAD > $(git rev-parse --git-dir)/shallow
-    git reflog expire --expire-unreachable=now --all
-    git reflog expire --expire=0
-    git reflog expire --expire=now --all
-    git prune
-    git prune-packed
+    #git show-ref -s HEAD > $(git rev-parse --git-dir)/shallow
+    #git reflog expire --expire-unreachable=now --all
+    #git reflog expire --expire=0
+    #git reflog expire --expire=now --all
+    #git prune
+    #git prune-packed
 
     cd "$currentAccessoriesDir"/parts/extendedInterface/
-    git reflog expire --expire-unreachable=now --all
-    git reflog expire --expire=now --all
-    git reflog expire --expire=0
-    git prune
-    git prune-packed
-    git gc --prune=all
-    git gc --aggressive
+    #git reflog expire --expire-unreachable=now --all
+    #git reflog expire --expire=now --all
+    #git reflog expire --expire=0
+    #git prune
+    #git prune-packed
+    #git gc --prune=all
+    #git gc --aggressive
     git gc
 
     _github_removeActionsHTTPS ./.git

756ff49ce2871370a1b61c4add8deaf47a50cce4
mirage335 <spamfreemirage335@gmail.com>
2023-09-04 06:05:01 -0400

    Draft.

--- _prog/core.sh
+++ _prog/core.sh
@@ -7,8 +7,18 @@ _experiment() {
 
 
 _setup_install-permissions() {
+	local functionEntryPWD
+	functionEntryPWD="$PWD"
+	
 	_messagePlain_probe_cmd chmod 755 /home/root/.ssh/id_*.pub
 	_messagePlain_probe_cmd chmod 600 /home/root/.ssh/id_*
+
+	cd /home/root/.ubcore/ubiquitous_bash
+	git config --global --add safe.directory /home/root/.ubcore/ubiquitous_bash
+	_gitMad
+
+
+	cd "$functionEntryPWD"
 }
 
 _setup_install-restore() {

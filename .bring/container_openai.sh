c32fa640278cc2152e4ac916884fb0fcc66c2e77
mirage335 <spamfreemirage335@gmail.com>
2025-12-04 00:15:43 +0000

    Draft.

---
+++ .bring/container_openai.sh
@@ -0,0 +1,196 @@
+#!/usr/bin/env bash
+
+# https://github.com/openai/codex-universal
+
+# Beware this script may only be given 5-10 minutes to run .
+
+
+git config --global checkout.workers -1
+#apt-get update -y
+#apt-get install -y apt-transport-https ca-certificates curl gnupg git wget
+
+
+
+(
+    apt-get update -y
+    sleep 0.3
+
+
+    cd
+
+    # Very limited code examples From 'unminimize' .
+    # https://packages.ubuntu.com/plucky/unminimize
+    # https://changelogs.ubuntu.com/changelogs/pool/main/u/unminimize/unminimize_0.5/copyright
+    # https://web.archive.org/web/20250605191859/https://changelogs.ubuntu.com/changelogs/pool/main/u/unminimize/unminimize_0.5/copyright
+    # https://packages.ubuntu.com/noble/unminimize
+    # https://changelogs.ubuntu.com/changelogs/pool/main/u/unminimize/unminimize_0.2/copyright
+    # https://web.archive.org/web/20250605192023/https://changelogs.ubuntu.com/changelogs/pool/main/u/unminimize/unminimize_0.2/copyright
+    # Copyright: 2024, Utkarsh Gupta <utkarsh@canonical.com>
+    # License: GPL-2.0+
+
+    #apt-get update -y
+    rm -f /etc/dpkg/dpkg.cfg.d/excludes
+    if [[ "$(dpkg-divert --truename /usr/bin/man)" = "/usr/bin/man.REAL" ]]; then
+        rm -f /usr/bin/man
+        dpkg-divert --quiet --remove --rename /usr/bin/man
+        sleep 0.3
+    fi
+    #
+    #apt-get update -y
+    apt-get install man-db manpages manpages-dev manpages-posix -y
+    sleep 0.3
+    #
+    _filter_noReinstall() {
+        grep -v 'icon\|x11\|dbus\|font\|cups\|freetype\|bzr\|gtk\|polkit\|xrender\|openjdk\|mime\|tzdata\|xkb\|xtrans' | grep -v 'perl\|gcc\|g++\|dpkg\|cpp\|bind9\|binutils\|clang\|yasm\|llvm\|glib\|java\|ruby\|gettext\|iputils\|inotify\|icu-devtools\|keybox\|libc-\|\libcurl\|libedit\|libffi\|libgdb\|libpam\|libpng\|libpcre\|libpq\|libpsl\|libxau\|xext\|libxft\|libxss\|lsb-release\|make\|moreutils\|curses\|protobuf\|rpcsvc\|software-properties-common\|common\|sgml\|tcl\|tk\|debconf\|debianutils\|e2fsprogs\|hostname\|helpers\|libpam\|login\|logsave\|sensible\|sysv\|dconf\|libcap2\|libcrypt\|libmysql\|rake\|ninja-build\|xdmcp\|udev\|libsystemd\|libstemmer\|libfm4\|libpango\|libmpfr6\|libmaxmind\|libtdl\|lliberc\|libldap2\|libipt\|libidn\|libgmp\|libgif\|libgdk\|pixbuf\|liggcl\|libfribidi\|libdb\|libdatrie\|libcbor\|libcairo\|libc6\|libasound\|libarchive\|libapt\|iso-codes\|libstdc\|distro-info-data\|ca-certificates' | grep -v 'xml\|swig\|tzlocal\|python3-yaml\|python3-wadlib\|python3-software-properties\|python3-psutil\|python3-patiencediff\|python3-oathlib\|python3-lazr\|launchpad\|python3-httplib2\|python-gi\|libtasn\|libsensors\|libpython3-stdlib\|libfm4\|python3-pygments\|build-essential' | grep -v 'pthread\|zlib1g\|lzma\|ccache\|pkgconf\|pkg-resources\|dirmngr'
+    }
+    #
+    #dpkg -S /usr/share/man/ |sed 's|, |\n|g;s|: [^:]*$||' | _filter_noReinstall | DEBIAN_FRONTEND=noninteractive xargs --no-run-if-empty apt-get install --reinstall -y > /quicklog.tmp 2>&1
+    #tail /quicklog.tmp
+    #rm -f /quicklog.tmp
+    #dpkg --verify --verify-format rpm | awk '$2 ~ /\/usr\/share\/doc/ {print $2}' | sed 's|/[^/]*$||' | sort | uniq | xargs --no-run-if-empty dpkg -S | sed 's|, |\n|g;s|: [^:]*$||' | uniq | _filter_noReinstall | DEBIAN_FRONTEND=noninteractive xargs --no-run-if-empty apt-get install --reinstall -y > /quicklog.tmp 2>&1
+    #tail /quicklog.tmp
+    #rm -f /quicklog.tmp
+    #
+    # ATTENTION: Reinstall documentation.
+    echo > packageList.tmp
+    #dpkg -S /usr/share/man/ |sed 's|, |\n|g;s|: [^:]*$||' | _filter_noReinstall > packageList.tmp
+    sleep 0.3
+    #dpkg --verify --verify-format rpm | awk '$2 ~ /\/usr\/share\/doc/ {print $2}' | sed 's|/[^/]*$||' | sort | uniq | xargs --no-run-if-empty dpkg -S | sed 's|, |\n|g;s|: [^:]*$||' | uniq | _filter_noReinstall >> packageList.tmp
+    sleep 0.3
+    cat packageList.tmp | uniq | DEBIAN_FRONTEND=noninteractive xargs --no-run-if-empty apt-get install --reinstall -y
+    sleep 0.3
+    #
+    #dpkg --verify --verify-format rpm | awk '$2 ~ /\/usr\/share\/doc/ {print " " $2}'
+    #
+    mandb -q
+    rm -f packageList.tmp
+
+
+
+
+    unset _aptGetInstall || true
+    #unalias _aptGetInstall 2>/dev/null || true
+    _aptGetInstall() {
+        env XZ_OPT="-T0" DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -q --install-recommends -y "$@"
+    }
+
+    _aptGetInstall procps strace sudo wget gpg curl pigz pixz bash aria2 git git-lfs bc nmap socat sockstat rsync net-tools uuid-runtime netcat-openbsd axel util-linux gawk libncurses-dev gh crudini bsdutils findutils p7zip p7zip-full unzip zip lbzip2 dnsutils bind9-dnsutils lz4 mawk patch tar gzip bzip2 sed pv iputils-ping zstd zlib1g coreutils openssl xz-utils libreadline8 libreadline-dev mkisofs genisoimage dos2unix lsof jq xxd sloccount dosfstools git-filter-repo qalc apt-utils | tail
+
+    #_aptGetInstall procps strace sudo wget gpg curl pigz pixz bash aria2 git git-lfs bc nmap socat sockstat rsync net-tools uuid-runtime netcat-openbsd axel unionfs-fuse util-linux screen gawk libelf-dev libncurses-dev gh crudini bsdutils findutils p7zip p7zip-full unzip zip lbzip2 jp2a dnsutils bind9-dnsutils lz4 mawk libelf-dev elfutils patch tar gzip bzip2 librecode0 udftools sed cpio pv expect wipe iputils-ping btrfs-progs btrfs-compsize zstd zlib1g coreutils openssl growisofs e2fsprogs xz-utils libreadline8 libreadline-dev mkisofs genisoimage wodim dos2unix fuse-overlayfs xorriso squashfs-tools mtools lsof aptitude jq xxd sloccount dosfstools apt-utils git-filter-repo qalc apt-transport-https tcl tk | tail
+
+    #_aptGetInstall procps strace sudo wget gpg curl pigz pixz bash aria2 git git-lfs bc nmap socat sockstat rsync net-tools uuid-runtime vim man-db gnulib libtool libtool-bin intltool libgts-dev netcat-openbsd iperf axel unionfs-fuse debootstrap util-linux screen gawk build-essential flex libelf-dev libncurses-dev autoconf libudev-dev dwarves pahole cmake gh libusb-dev libusb-1.0 setserial libffi-dev libusb-1.0-0 libusb-1.0-0-dev libusb-1.0-doc pkg-config crudini bsdutils findutils v4l-utils libevent-dev libjpeg-dev libbsd-dev libusb-1.0 gdb libbabeltrace1 libc6-dbg libsource-highlight-common libsource-highlight4v5 initramfs-tools dmidecode p7zip p7zip-full unzip zip lbzip2 jp2a dnsutils bind9-dnsutils live-boot mktorrent gdisk lz4 mawk nano bison libelf-dev elfutils patch tar gzip bzip2 librecode0 sed texinfo udftools wondershaper sysbench libssl-dev cpio pv expect libfuse2 wipe iputils-ping btrfs-progs btrfs-compsize zstd zlib1g nilfs-tools coreutils sg3-utils kpartx openssl growisofs udev cryptsetup parted e2fsprogs xz-utils libreadline8 libreadline-dev mkisofs genisoimage wodim eject hdparm sdparm php cifs-utils debhelper nsis dos2unix fuse-overlayfs xorriso squashfs-tools grub-pc-bin grub-efi-amd64-bin mtools squashfs-tools squashfs-tools-ng fdisk lsof usbutils aptitude recode libpotrace0 libwmf-bin w3m par2 yubikey-manager qrencode tasksel jq xxd sloccount dosfstools apt-utils git-filter-repo qalc apt-transport-https tcl tk libgdl-3-5 libgdl-3-common | tail
+) &
+sleep 1
+
+
+
+
+# ### _getCore_ub
+if [[ ! -e ~/core/infrastructure/ubiquitous_bash ]]
+then
+    mkdir -p ~/core/infrastructure
+    cd ~/core/infrastructure
+    git clone --depth 1 --recursive https://github.com/mirage335-colossus/ubiquitous_bash.git
+fi
+cd ~/core/infrastructure/ubiquitous_bash
+#~/core/infrastructure/ubiquitous_bash/ubiquitous_bash.sh _gitBest pull
+
+[[ ! -e /workspace/ubiquitous_bash ]] && cp -a ~/core/infrastructure/ubiquitous_bash /workspace/ubiquitous_bash
+
+
+
+
+(
+    cd
+    cd /workspace/extendedInterface
+    ~/core/infrastructure/ubiquitous_bash/ubiquitous_bash.sh _gitBest submodule update --init --depth 1 --recursive
+)
+
+
+
+
+
+
+# Certificates installation by '_setupUbiquitous' may otherwise cause dpkg lock multi-threading/concurrency collision.
+sleep 1
+wait
+
+
+if [[ ! -e ~/.ubcore/ubiquitous_bash ]]
+then
+    cd ~/core/infrastructure/ubiquitous_bash
+    ./_setupUbiquitous.bat
+fi
+cd ~/.ubcore/ubiquitous_bash
+#~/.ubcore/ubiquitous_bash/ubiquitous_bash.sh _gitBest pull
+cd
+#export profileScriptLocation="/root/core/infrastructure/ubiquitous_bash/ubiquitous_bash.sh"
+#export profileScriptFolder="/root/core/infrastructure/ubiquitous_bash"
+#. "/root/core/infrastructure/ubiquitous_bash/ubiquitous_bash.sh" --profile _importShortcuts
+
+
+
+
+sleep 1
+wait
+
+
+
+
+
+cd
+#~/.ubcore/ubiquitous_bash/ubiquitous_bash.sh _setup_codex
+
+if false
+then
+# ###
+cd
+curl -fsSL https://ollama.com/install.sh | sh
+sudo -n -u ollama ollama serve &
+#
+#wget 'https://huggingface.co/mlabonne/Meta-Llama-3.1-8B-Instruct-abliterated-GGUF/resolve/main/meta-llama-3.1-8b-instruct-abliterated.Q4_K_M.gguf'
+#aria2c --log=- --log-level=info -x "3" --async-dns=false -o 'llama-3.1-8b-instruct-abliterated.Q4_K_M.gguf' 'https://huggingface.co/mlabonne/Meta-Llama-3.1-8B-Instruct-abliterated-GGUF/resolve/main/meta-llama-3.1-8b-instruct-abliterated.Q4_K_M.gguf'
+#[[ ! -e 'llama-3.1-8b-instruct-abliterated.Q4_K_M.gguf' ]] && aria2c --log=- --log-level=info -x "3" --async-dns=false -o 'llama-3.1-8b-instruct-abliterated.Q4_K_M.gguf' 'https://huggingface.co/mlabonne/Meta-Llama-3.1-8B-Instruct-abliterated-GGUF/resolve/main/meta-llama-3.1-8b-instruct-abliterated.Q4_K_M.gguf' --disable-ipv6=true
+~/.ubcore/ubiquitous_bash/ubiquitous_bash.sh _wget_githubRelease_join "soaringDistributions/Llama-3-augment_bundle" "" "llama-3.1-8b-instruct-abliterated.Q4_K_M.gguf"
+#
+echo 'FROM ./llama-3.1-8b-instruct-abliterated.Q4_K_M.gguf
+PARAMETER num_ctx 6144
+' > Llama-3-augment.Modelfile
+~/.ubcore/ubiquitous_bash/ubiquitous_bash.sh _here_license-Llama-3-augment >> Llama-3-augment.Modelfile
+#
+ollama create Llama-3-augment -f Llama-3-augment.Modelfile
+rm -f Llama-3-augment.Modelfile
+rm -f llama-3.1-8b-instruct-abliterated.Q4_K_M.gguf
+# ###
+fi
+
+
+
+
+#sleep 1
+#wait
+
+
+
+
+
+cd
+
+#~/.ubcore/ubiquitous_bash/ubiquitous_bash.sh _getMinimal_special
+#~/.ubcore/ubiquitous_bash/ubiquitous_bash.sh _getMinimal_cloud
+#~/.ubcore/ubiquitous_bash/ubiquitous_bash.sh _getMost
+
+
+
+export devfast=true
+export skimfast=true
+
+export ub_setScriptChecksum_disable='true'
+export ubDEBUG=true
+
+
+
+unset ubiquitousBashID || true
+
+uptime
+echo ' request: _setup_codex ; _setup_ollama ; apt-get install ... _getMinimal_special comments... '
---
+++ AGENTS.md
@@ -0,0 +1,171 @@
+
+---
+
+BEGIN directory specific "extendedInterface" AGENTS.md , other input may regard other hierarchical directories.
+
+The extendedInterface project is a 'fork' derivative of 'ubiquitous_bash' using the shell script functions, compile, etc, of 'ubiquitous_bash' as a library. To understand how this project works, please also read the ./_lib/ubiquitous_bash/AGENTS.md file .
+
+
+# Notable "extendedInterface" Subdirectories and Files
+
+## _prog
+
+Most bash shellcode specific to extendedInterface . Compiled into 'ubiquitous_bash.sh' .
+
+## _lib/nsis/extIface.nsi
+
+The build for the MSWindows installer. Because the installer build runs under UNIX/Linus, and because the installer itself early installs a 'ubcp' Cygwin environment, 'ubiquitous_bash' bash shell script functions are called for both the build process and to carry out some of the installation steps.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Mechanisms of "ubiquitous_bash"
+
+Calling './ubiquitous_bash.sh' with a '_function' as the first parameter will call that internal function with remaining parameters given to that function.
+
+## Compiling "ubiquitous_bash and Forked Derivatives
+
+Script 'ubiquitous_bash.sh', etc, is compiled by 'compile.sh' from the framework of most of the shellscript files from the ubiquitous_bash repository. Since these framework shellscript files are concatenated, and are usually not independently executable, a '#!/usr/bin/env bash' header, etc, would cause errors - the '*.sh' filename is mostly used to get correct syntax highlighting in text apps.
+
+Forked derivative projects similarly compile 'ubiquitous_bash.sh' or similarly named scripts from shellscript files in their subdirectories (usually '_prog') as well as the upstream "ubiquitous_bash" framework shellscript files (usually '_lib/ubiquitous_bash'). Shellscript files from the derivative fork project are compiled to a later position in the "ubiquitous_bash" compiled script, overriding same named upstream functions.
+
+## Anchor Shortcut Scripts
+
+Anchor scripts '_anchor.bat', etc, are vaguely equivalent to '.lnk' or '.desktop' shortcut files, calling a function of the same name as their filename basename in the "ubiquitous_bash" compiled script. Both MSWindows batch and UNIX/Linux shell comment characters are interleaved in the first lines of the batch anchor scripts to run as a batch file under MSWindows (calling bash.exe from portable Cygwin environment if needed) and as a bash shell script under UNIX/Linux .
+
+Function _anchor calls several more functions, including a _refresh_anchors function and other functions which may be defined in a forked derivative project to copy the template _anchor and _anchor.bat files after setting a couple of variables with sed commands.
+
+
+# Development Practices, Style, Tracing, Testing Expectations, Uniquely Necessary for "ubiquitous_bash"
+
+## Sandbox, Harness
+
+Never sandbox , drop root , or otherwise limit any command calling "ubiquitous_bash" scripts or functions , such as './ubiquitous_bash.sh _test' , './app _test' , './_test' , './_test.bat' , './_bin' , './bin.bat' , './ubiquitous_bash.sh _bin' , etc.
+
+Hardcoded platform-specific temporary directory paths , home directories , /tmp temporary subdirectories , /dev/shm temporary subdirectories , root commands , sudo , etc, are all necessary for "ubiquitous_bash" to function . Usually "ubiquitous_bash" either runs within an appropriately limited ephemeral container , dedicated computer , etc, with already appropriately limited capabilities, and enforces its own limits, sanity checks, etc, or "ubiquitous_bash" uses root commands to create a distribution / Operating System for such limited ephemeral situations. In both cases, it is appropriate for "ubiquitous_bash" scripts, commands, functions, etc, to run with root privileges.
+
+Docker containers in particular may be intended to begin with the root user by default , as the situation most expected and supported by the developers of such Docker containers , do not attempt to run commands as a different user or otherwise drop root permissions.
+
+Always run "ubiquitous_bash" scripts, commands, functions, etc, with the permissions of the user running the Codex process, without limits on what directories may be written to, etc.
+
+If such calls as creating a subdirectory of the "$HOME" directory fail, such as  mkdir -p /root/.ubtmp/try  ,  bash -c 'touch /root/.ubtmp/testfile'  failing due to 'Permission denied' , attempt to test whether such limitations as bash shells not having write access to the "$HOME" directory may be obstructing . If such sandboxing may be obstructing, quit and report the issue to the user. Do not continue if sandboxing may be obstructing .
+
+
+## Robustness, Resilience
+
+Avoid suffering dependency and availability issues by preferring to use commands from external programs, libraries, etc, using not older or abandoned codebases, dependencies, etc, but the more actively developed codepaths. Recently frozen dependencies are more likely still available than dependencies for eventually forked older code. Developers with their own non-reproducible build environments are more likely to notice when freezing dependencies becomes unsustainable or when unfrozen dependencies break things in their own development work.
+
+## Style
+
+Abstraction, randomized temporary directories, process management, recursion, commented code, uncommented self-explanatory code, absence of debugging output, or very verbose default debugging output, can be uniquely appropriate throughout the "ubiquitous_bash" codebase. Keeping many external programs interoperable necessitates some emphasis on rapid reaction to external changes, a relatively thorough, drastic, approach to maintenance, to the point that too many comments or debug output statements can obscure and distract from the structure of the code which a person experienced in such work would see visually in a text editing app at a glance. Too much code or absence of alternative code could also take longer to rewrite, especially if iteration through many rewrites is necessary due to undocumented complexities.
+
+Do not assume "ubiquitous_bash" follows conventional patterns, it doesn't.
+
+## Development Tracing
+
+Temporary changes to shellscripts, adding echo statements, etc, is often necessary to observe progress, detect actual hanging, and diagnose failures, especially with multi-threaded, recursive, etc, scripts. Better to anticipate this and add statements than to waste time terminating an experiment that was not actually hanging.
+
+Tracing ubiquitous_bash shell scripts can be done efficiently with  'export ubDEBUG=true'  , tracing all commands and shell script function calls within a function given as a command line parameter. Setting  'export ubDEBUG=true'  only increases verbosity, echo to STDOUT, etc, will still be called.
+
+## Testing Expectations
+
+Please base your expectations on plausible outcomes from enumerating stepwise processing of plausible inputs through code of relevant functions, etc . Delays are reasonable for such timing sensitive or high latency tests as subsecond sleep commands, inter-process communication, command runtime measurement, etc . Excessive looping is also reasonable as the tests performed may be particularly extreme, catching the slightest changes to syntax over years of new interpreter, compiler, etc, versions. Such looping may in fact be repetitive use of very similar or even the same syntax for testing purposes.
+
+A '_test' function may run successfully for tens of minutes, possibly a few hours, without any output, if the function was written to avoid any commands that could actually hang, and to only output a single message 'Testing...' followed by either 'PASS' if successful, or an exit status with a few error messages from the failed command. Sub-functions called by a larger '_test' function may output only a successful exit status, delegating the 'PASS' message to only be output by the larger '_test' function after other sub-functions have been successful as well. Unusual error messages are made more noticeable by not outputting noisy status messages during sanity tests often used during Continuous Integration and installation. Sanity and dependency tests, etc, far more extensive with "ubiquitous_bash" than with other projects, have proven necessary to detect changes made to software versions such as the bash interpreter.
+
+Keep going through such testing until a definitive result is reached, with a 'PASS' or similar message, an explainable non-error exit status, etc.
+
+An extended or indefinite run is very acceptable as long as information gathering continues and progress does not cease entirely. CPU usage may be either high due to bash shell interpreter overhead testing many conditions or other unusual syntax, or CPU usage may be low due to sleep , etc , intentionally having been put into the script for Inter-Process Communication latency timing, deliberately reducing CPU usage, etc.
+
+Both STDOUT and STDERR may provide useful information, especially if verbosity is increased to assist testing, etc, such as with  'export ubDEBUG=true'  .
+
+## Direct Editing
+
+Although Git Pull Requests should instead edit the underlying files, directly editing the otherwise compiled 'ubiquitous_bash.sh' script is an acceptable technique for testing, diagnosing, experimenting, etc. Disable checksum with  'export ub_setScriptChecksum_disable=true'  to run the script after editing without recompiling.
+
+## Terminology
+
+- procedure
+- sequence
+
+- start
+- stop
+
+- begin
+- end
+
+```bash
+_function_procedure() {
+    echo "PASS" > "$safeTmp"/status
+    #echo "FAIL" > "$safeTmp"/status
+
+    cat "$safeTmp"/status
+}
+
+_function_sequence() {
+    _start
+
+    _function_procedure "$@"
+
+    _stop
+}
+
+"$scriptAbsoluteLocation" _function_sequence "$@"
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+END directory specific "ubDistBuild" AGENTS.md , other input may regard other hierarchical directories.
+
+---
